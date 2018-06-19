58abe71158a31e538abb9112c505f98998393255
amartdom <amartdom@gmail.com>
2018-06-19 18:57:49 +0200

    Añadidas pantallas

--- app/src/main/java/com/example/angelmartindominguez/flavourtester/MainActivity.java
+++ app/src/main/java/com/example/angelmartindominguez/flavourtester/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.angelmartindominguez.flavourtester;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        controlador();
+    }
+
+    public void controlador(){
+
+        Button entry =  findViewById(R.id.enter);
+
+        //hago clic y se abre el 2
+        entry.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent menu = new Intent(getApplicationContext(), MenuLateral.class);
+                startActivity(menu);
+            }
+        });
     }
 }
